6e8c6dddfe84d513439c6ae7bd7db3f072706861
Oran Rush <oranrush@Orans-MacBook-Pro.local>
2019-03-04 18:48:25 -0600

    Upload2

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,24 +10,70 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
+    if (err) console.log(err);
     console.log("Connected as id: " + connection.threadId);
-
+    amazonSearch();
 });
 
 
-var start = function() {
+function amazonSearch() {
     inquirer.prompt({
         name: "idOrUnits",
         type: "input",
-        message: "What is the ID of the product you would like to buy?",
-                "How many units would like to buy?",
-    }).then(function(answer) {
-        if(answer.idOrUnits) {
-            console.log(answer.idOrUnits);
-        } else {
+        message: [ 
+            "What is the ID of the product you would like to buy?",
+            "How many units would you like to buy?"
+            ]
+    })
+    .then(function(answer) {
+        // if(answer.idOrUnits) {
+        //     console.log(answer.idOrUnits);
+        // } else {
             
+        // }
+
+        switch (answer.action) {
+        case "What is the ID of the product you would like to buy?":
+        productSearch();
+        break;
+
+        case "How many units would you like to buy?":
+        unitSearch();
+        break;
         }
-
     });
+}
 
+function productSearch() {
+    inquirer
+        .prompt ({
+        name: "product",
+        type: "input",
+    })
+   .then(function(answer) {
+    var query = "Select product name?";
+    connection.query(query, {product_name: answer.product }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].prooduct);
+        }
+        amazonSearch();
+    });
+   });
+}
+
+function unitSearch() {
+    inquirer
+        .prompt ({
+        name: "units",
+        type: "input",
+    })
+   .then(function(answer) {
+    var query = "Select number of units?";
+    connection.query(query, {stock_quanity: answer.units }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].units);
+        }
+        amazonSearch();
+    });
+   });
 }
