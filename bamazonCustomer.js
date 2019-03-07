d13d8b46b2ee995f73514cf0cec84fed6064656f
Oran Rush <oranrush@Orans-MBP.lan1>
2019-03-07 12:06:24 -0600

    Upload4

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+
+// mysql connection
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -16,75 +18,65 @@ connection.connect(function(err) {
 });
 
 
+function viewProducts(products) {
+    connection.query("SELECT * FROM bamazonDB.products", function(err, res) {
+        if(err) throw err;
+
+        console.log("Welcome To The BAmazon Store");
+        console.log("=====================================");
+
+        for(var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Stock Quanity: " + res[i].stock_quantity);
+            console.log("======================================================================");
+        }
+    },
+
+
+
+// The user messages
 function amazonSearch() {
     inquirer.prompt({
-        name: "idOrUnits",
+        name: "product_id",
         type: "input",
         message: [ 
             "What is the ID of the product you would like to buy?",
-            "How many units would you like to buy?"
             ]
     })
     .then(function(answer) {
-        // if(answer.idOrUnits) {
-        //     console.log(answer.idOrUnits);
-        // } else {
-            
-        // }
-
-        switch (answer.action) {
-        case "What is the ID of the product you would like to buy?":
-        productSearch();
-        break;
-
-        case "How many units would you like to buy?":
-        // unitSearch();
-        break;
-        }
-    });
-}
+    if (answer.product_id) { //check if user entered product_id
+        productSearch(answer.product_id); //call the function that searches for the product in the DB
+    }
+});
+},
 
 
-
-function productSearch() {
+// Place the order and check to see if store has enough
+function productSearch(product_id) {
     inquirer
         .prompt ({
-        name: "product",
+        name: "quantity",
         type: "input",
+        message: ["How many units would you like to buy?"]
     })
    .then(function(answer) {
-    var query = "SELECT * FROM bamazonDB.products";
-    connection.query(query, {item_id: answer.product }, function(err, res) {
+    var query = "SELECT * FROM bamazonDB.products WHERE item_id = ? AND stock_quantity > ?";
+    connection.query(query, [product_id, answer.quantity], function(err, res) {
+        console.log(err, res);
+        if (res.length > 0) { //if there are results - output products
         for (var i = 0; i < res.length; i++) {
             console.log(
                 "Item: " + res[i].item_id + 
                 " || Product Name: " + res [i].product_name + 
                 " || Departmnet Name: " + res [i].department_name + 
                 " || Price: " + res [i].price +
-                " || Stock Quanity: " + res [i].stock_quanity
+                " || Stock Quanity: " + res [i].stock_quantity
                 );
         }
-        amazonSearch();
+    }
+    else { // if no results - error
+        console.log('Not enough inventory!');
+        }
+        // amazonSearch();
     });
    });
 }
-
-
-
-
-// function unitSearch() {
-//     inquirer
-//         .prompt ({
-//         name: "units",
-//         type: "input",
-//     })
-//    .then(function(answer) {
-//     var query = "Select number of units?";
-//     connection.query(query, {stock_quanity: answer.units }, function(err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//             console.log(res[i].units);
-//         }
-//         amazonSearch();
-//     });
-//    });
-// }
